0a68970d3afe8880f3ad12d73465b7cad43a5f6b
ALICIA <alicia.magro@gmail.com>
2025-01-27 17:06:09 +0100

    front router is DONE

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
@@ -6,9 +7,15 @@ function NavBar() {
       <nav className="nav">
         <img src="./logo.png" alt="hogwarts logo" className="logo-nav" />
         <ul className="nav-list">
-          <li className="nav-items">Accueil</li>
-          <li className="nav-items">Matières</li>
-          <li className="nav-items">Maisons</li>
+          <Link to="/">
+            <li className="nav-items">Accueil</li>
+          </Link>
+          <Link to="/subjects">
+            <li className="nav-items">Matières</li>
+          </Link>
+          <Link to="/houses">
+            <li className="nav-items">Maisons</li>
+          </Link>
         </ul>
       </nav>
     </header>
