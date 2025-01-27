c01224b67bbb54a500a58285f04f5427679aea1c
ALICIA <alicia.magro@gmail.com>
2025-01-27 16:52:07 +0100

    started the houses page

---
+++ client/src/pages/Houses/Houses.tsx
@@ -0,0 +1,11 @@
+import "./Houses.css";
+
+function Houses() {
+  return (
+    <main className="houses-container">
+      <h1 className="houses-title">Maisons</h1>
+    </main>
+  );
+}
+
+export default Houses;
