d83009692d2706954987b7879a6e732c97124d5c
ALICIA <alicia.magro@gmail.com>
2025-01-27 14:17:11 +0100

    started the navbar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -2,18 +2,13 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Accueil</a>
-          </li>
-          <li>
-            <a href="/about">Matières</a>
-          </li>
-          <li>
-            <a href="/contact">Maisons</a>
-          </li>
+    <header className="nav-container">
+      <nav className="nav">
+        <img src="./logo.png" alt="hogwarts logo" className="logo-nav" />
+        <ul className="nav-list">
+          <li className="nav-items">Accueil</li>
+          <li className="nav-items">Matières</li>
+          <li className="nav-items">Maisons</li>
         </ul>
       </nav>
     </header>
