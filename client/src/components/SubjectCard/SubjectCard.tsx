d7546a2d766caa58451e3b0cb77d1c7b75e7b1d7
ALICIA <alicia.magro@gmail.com>
2025-02-12 15:28:57 +0100

    subjects page is done

--- client/src/components/SubjectCard/SubjectCard.tsx
+++ client/src/components/SubjectCard/SubjectCard.tsx
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-// type Subject = {
-//   id: number;
-//   img: string;
-//   name: string;
-//   description: string;
-// };
-
 type Subject = {
   id: number;
   img: string;
@@ -20,23 +13,53 @@ interface SubjectCardProps {
 
 function SubjectCard({ subject }: SubjectCardProps) {
   const [subjectDetails, setSubjectDetails] = useState(false);
+  const [popUp, setPopUp] = useState(false);
+
+  const handleClickPopUp = () => {
+    setPopUp(!popUp);
+  };
 
   const handleClick = () => {
     setSubjectDetails(!subjectDetails);
   };
 
+  const handleClosePopUp = () => {
+    setPopUp(false);
+  };
+
   return (
-    <article className="all-subjects" key={subject.id}>
+    <section className="all-subjects" key={subject.id}>
       <img
         src={subject.img}
         alt={subject.name}
         className="subject-img"
-        onClick={handleClick}
-        onKeyUp={handleClick}
+        onClick={() => {
+          handleClick();
+          handleClickPopUp();
+        }}
+        onKeyUp={() => {
+          handleClick();
+          handleClickPopUp();
+        }}
       />
       <h3>{subject.name}</h3>
-      {subjectDetails && <p>{subject.description}</p>}
-    </article>
+      {popUp && subjectDetails && (
+        <article className="pop-up-article">
+          <div className="pop-up-container">
+            <p className="pop-up-description">
+              {subject.description}{" "}
+              <p
+                className="close-button"
+                onClick={handleClosePopUp}
+                onKeyUp={handleClosePopUp}
+              >
+                X
+              </p>
+            </p>
+          </div>
+        </article>
+      )}
+    </section>
   );
 }
 
--- client/src/pages/Subject/Subject.tsx
+++ client/src/pages/Subject/Subject.tsx
@@ -34,6 +34,54 @@ function Subject() {
           <SubjectCard subject={subject} key={subject.name} />
         ))}
       </section>
+      <section className="more-details">
+        <h2 className="second-title-subjects"> Les B.U.S.E.S</h2>
+        <article className="subject-more-details">
+          Les B.U.S.E.S (Brevet Universel de Sorcellerie Élémentaire) sont des
+          examens passés par les élèves de cinquième année. Ils déterminent les
+          matières que les élèves pourront continuer en sixième et septième
+          années, en fonction de leurs résultats. Les notes possibles vont de O
+          (Optimal) à T (Troll), avec des mentions comme Effort Exceptionnel (E)
+          et Acceptable (A) pour les réussites, tandis que Piètre (P), Désolant
+          (D) et Troll (T) signifient l'échec.
+        </article>
+        <h2 className="second-title-subjects"> Les A.S.P.I.C.S</h2>
+        <article className="subject-more-details">
+          Les A.S.P.I.C. (Accumulation de Sorcellerie Particulièrement Intensive
+          et Contraignante) sont les examens finaux passés en septième année à
+          Poudlard. Ils sont l'équivalent des B.U.S.E., mais à un niveau bien
+          plus avancé, et jouent un rôle clé pour l'avenir professionnel des
+          sorciers. Comme pour les B.U.S.E., les notes vont de Optimal (O) à
+          Troll (T). Certains métiers, comme Auror, nécessitent d’excellents
+          résultats dans plusieurs matières.
+        </article>
+      </section>
+      <h2 className="second-title-subjects"> À savoir...</h2>
+      <article className="specific-details">
+        Jusqu'en cinquième année, certains cours sont obligatoires :
+        <ul>
+          <li>Potions</li>
+          <li>Botanique</li>
+          <li>Sortilèges</li>
+          <li>Métamorphose</li>
+          <li>Histoire de la magie</li>
+          <li>Défense contre les forces du Mal</li>
+          <li>Astronomie</li>
+        </ul>
+        Par la suite, ce sont vos résultats aux <b>BUSES</b> que vous passerez à
+        la fin de votre cinquième année qui détermineront les matières que vous
+        pourrez continuer, ou non.
+        <br />
+        De plus, c'est en troisième année qu'il vous sera demandé de choisir au
+        moins deux de ces matières optionnelles :
+        <ul>
+          <li>Divination</li>
+          <li>Soins aux créatures magiques</li>
+          <li>Étude des Moldus</li>
+          <li>Étude des Runes Anciennes</li>
+          <li>Arithmancie</li>
+        </ul>
+      </article>
     </main>
   );
 }
